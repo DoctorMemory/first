a5cdff05ab868754fb02f5b9c81a1376e13d556b
Brian Watkins <watkinsbrianr@gmail.com>
2009-09-08 23:53:24 -0400

    modified config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "set your application name here"
+set :application, "test1"
 set :repository,  "set your repository location here"
 
 # If you have previously been relying upon the code to start, stop 
@@ -7,26 +7,26 @@ set :repository,  "set your repository location here"
 
 # If you are deploying a rails app you probably need these:
 
-# load 'ext/rails-database-migrations.rb'
-# load 'ext/rails-shared-directories.rb'
+  load 'ext/rails-database-migrations.rb'
+  load 'ext/rails-shared-directories.rb'
 
 # There are also new utility libaries shipped with the core these 
 # include the following, please see individual files for more
 # documentation, or run `cap -vT` with the following lines commented
 # out to see what they make available.
 
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+  load 'ext/spinner.rb'              # Designed for use with script/spin
+  load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+  load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+  set :deploy_to, "/home/brianwat/rails/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+  set :scm, :git
 # see a full list by running "gem contents capistrano | grep 'scm/'"
 
-role :web, "your web-server here"
+role :web, "brian-watkins.us"
