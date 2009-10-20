68debb81a6281bccc79dae801f89f114c9e54f1f
Brian Watkins <watkinsbrianr@gmail.com>
2009-10-19 23:18:52 -0400

    modifed deploy.rb app is now first

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :user, "brianwat"
-set :application, "test1"
+set :application, "first"
 set :use_sudo, false
 set :deploy_to, "/home/brianwat/rails/#{application}"
 set :repository, "http://github.com/DoctorMemory/first.git"
